c1530879239e0a66e5d8e7749c76f7908c4f4058
Yan Nascimento <75856666+yanunes11@users.noreply.github.com>
2021-03-29 12:23:56 -0300

    Update matematica.py

--- matematica.py
+++ matematica.py
@@ -1,1 +1,20 @@
-print('Top')
+#ENTRA COM NUMERO
+#CALCULA
+
+def fatorial_yan (numero):
+    print('---FATORIAL---')
+    #num = int(input('Digite um número inteiro: '))
+    num = int(numero)
+    fat = []
+    soma = 1
+    for i in range(num, 0, -1):
+        fat.append(i)
+    for i in range(0, num):
+        soma *= fat[i]
+    print(f'{num}! =', end='')
+    for i in range(0, num):
+        if fat[i] != 1:
+            print(f' {fat[i]} X', end='')
+        else:
+            print(f' {fat[i]} = ', end='')
+    print(soma)
